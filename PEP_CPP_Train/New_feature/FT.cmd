eeee4a71c98d86e0eabc7e1e5aee1c91b580497e
zczczc777 <32671863+zczczc777@users.noreply.github.com>
2021-01-26 13:16:24 +0800

    Add files via upload

---
+++ PEP_CPP_Train/New_feature/FT.cmd
@@ -0,0 +1,5 @@
+@echo off
+C:
+cd C:\Users\Administrator\Desktop\PEP_Train/
+C:\Users\Administrator\Desktop\PEP_Train//New_feature/mrmr_win32.exe -i C:\Users\Administrator\Desktop\PEP_Train//New_feature/FTc.csv -n 99 >C:\Users\Administrator\Desktop\PEP_Train//New_feature/FT.mrmrout
+exit
